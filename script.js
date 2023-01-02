6e8ca3e8e0ab1b56cbef0dd7182cbeac427ccbe7
ViniSouza01 <viniciussouza130705@gmail.com>
2023-01-02 20:54:21 -0300

    alteração menu mobile

---
+++ script.js
@@ -0,0 +1,6 @@
+function menu() {
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+    const div = document.getElementById('btn-mobile');
+    div.classList.toggle('active');
+}
